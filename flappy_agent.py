e0db2c8da8becbac6ff8aa4ac4ec55edd4de99cc
Unknown <sverrirs15@ru.is>
2017-10-27 00:49:56 +0000

    Auto stash before merge of "master" and "origin/master"

--- flappy_agent.py
+++ flappy_agent.py
@@ -85,27 +85,10 @@ class FlappyAgentMCAverage(FlappyAgent):
         self.discount = 1
         self.epsilon = 0.1
 
-    # Q(s,a) := (Q, s, a) + 1/n(s,a) * [G - Q(s,a)]
-
     def reward_values(self):
-        """ returns the reward values used for training
-
-            Note: These are only the rewards used for training.
-            The rewards used for evaluating the agent will always be
-            1 for passing through each pipe and 0 for all other state
-            transitions.
-        """
         return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
 
     def observe(self, s1, a, r, end):
-        """ this function is called during training on each step of the game where
-            the state transition is going from state s1 with action a to state s2 and
-            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
-
-            Unless a terminal state was reached, two subsequent calls to observe will be for
-            subsequent steps in the same episode. That is, s1 in the second call will be s2
-            from the first call.
-            """
         self.observations.append((s1, a, r))
         if end:
             G = 0
@@ -129,13 +112,6 @@ class FlappyAgentMCAverage(FlappyAgent):
             self.observations = []
 
     def training_policy(self, state):
-        """ Returns the index of the action that should be done in state while training the agent.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            training_policy is called once per frame in the game while training
-        """
-        # TODO: change this to to policy the agent is supposed to use while training
-
         actions = [0, 1]
 
         greedy_action = self.pi[state]
@@ -146,14 +122,6 @@ class FlappyAgentMCAverage(FlappyAgent):
             return [x for x in actions if x != greedy_action][0]
 
     def policy(self, state):
-        """ Returns the index of the action that should be done in state when training is completed.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            policy is called once per frame in the game (30 times per second in real-time)
-            and needs to be sufficiently fast to not slow down the game.
-        """
-        # TODO: change this to to policy the agent has learned
-        # At the moment we just return an action uniformly at random.
         return self.pi[state]
 
     # Gets a state in the original format that PyGame returns.
@@ -202,27 +170,10 @@ class FlappyAgentMCLearningRate(FlappyAgent):
         self.epsilon = 0.1
         self.learning_rate = LearningRate
 
-    # Q(s,a) := (Q, s, a) + 1/n(s,a) * [G - Q(s,a)]
-
     def reward_values(self):
-        """ returns the reward values used for training
-
-            Note: These are only the rewards used for training.
-            The rewards used for evaluating the agent will always be
-            1 for passing through each pipe and 0 for all other state
-            transitions.
-        """
         return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
 
     def observe(self, s1, a, r, end):
-        """ this function is called during training on each step of the game where
-            the state transition is going from state s1 with action a to state s2 and
-            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
-
-            Unless a terminal state was reached, two subsequent calls to observe will be for
-            subsequent steps in the same episode. That is, s1 in the second call will be s2
-            from the first call.
-            """
         self.observations.append((s1, a, r))
         if end:
             G = 0
@@ -239,13 +190,6 @@ class FlappyAgentMCLearningRate(FlappyAgent):
             self.observations = []
 
     def training_policy(self, state):
-        """ Returns the index of the action that should be done in state while training the agent.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            training_policy is called once per frame in the game while training
-        """
-        # TODO: change this to to policy the agent is supposed to use while training
-
         actions = [0, 1]
 
         greedy_action = self.pi[state]
@@ -256,14 +200,6 @@ class FlappyAgentMCLearningRate(FlappyAgent):
             return [x for x in actions if x != greedy_action][0]
 
     def policy(self, state):
-        """ Returns the index of the action that should be done in state when training is completed.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            policy is called once per frame in the game (30 times per second in real-time)
-            and needs to be sufficiently fast to not slow down the game.
-        """
-        # TODO: change this to to policy the agent has learned
-        # At the moment we just return an action uniformly at random.
         return self.pi[state]
 
     # Gets a state in the original format that PyGame returns.
@@ -283,39 +219,22 @@ class FlappyAgentMCLearningRate(FlappyAgent):
         return y_pos, top_y_gap, horizontal_distance_next_pipe, velocity
 
 def run_game(nb_episodes, agent):
-    """ Runs nb_episodes episodes of the game with agent picking the moves.
-        An episode of FlappyBird ends with the bird crashing into a pipe or going off screen.
-    """
-
-    #reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
-    # TODO: when training use the following instead:
     reward_values = agent.reward_values()
     
     env = PLE(FlappyBird(), fps=30, display_screen=False, force_fps=True, rng=None,
               reward_values=reward_values)
-    # TODO: to speed up training change parameters of PLE as follows:
-    # display_screen=False, force_fps=True 
     env.init()
 
     score = 0
     while nb_episodes > 0:
-        # pick an action
-        # TODO: for training using agent.training_policy instead
         state = agent.parse_state(env.game.getGameState())
-
         action = agent.training_policy(state)
-
-        # step the environment
         reward = env.act(env.getActionSet()[action])
-        #print("reward=%d" % reward)
-
-        # TODO: for training let the agent observe the current state transition
-
-        score += reward
 
         agent.observe(state, action, reward, env.game_over())
 
-        # reset the environment if the game is over
+        score += reward
+
         if env.game_over():
             #print("score for this episode: %d" % score)
             env.reset_game()
@@ -353,10 +272,8 @@ def test_policy(nb_episodes, agent):
     print("Average: %d" % (sum(scores)/len(scores)))
 
 
-
-
 agent = FlappyAgentMCLearningRate(0.1)
-#run_game(50000, agent)
+# run_game(50000, agent)
 pi = numpy.load("Average_Policy_50000.npy").item()
 agent.pi = pi
 test_policy(30, agent)
