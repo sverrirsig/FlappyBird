48b077c5cfd6625ef119926fc3aadb5d7e691e75
Unknown <sverrirs15@ru.is>
2017-10-31 02:19:29 +0000

    Refractor of death

--- flappy_agent.py
+++ flappy_agent.py
@@ -3,72 +3,16 @@ from ple import PLE
 import random
 import numpy
 import itertools
-import time
-import os
-import re
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 class FlappyAgent:
     def __init__(self):
-        # TODO: you may need to do some initialization for your agent here
-        return
-    
-    def reward_values(self):
-        """ returns the reward values used for training
-        
-            Note: These are only the rewards used for training.
-            The rewards used for evaluating the agent will always be
-            1 for passing through each pipe and 0 for all other state
-            transitions.
-        """
-        return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
-    
-    def observe(self, s1, a, r, s2, end):
-        """ this function is called during training on each step of the game where
-            the state transition is going from state s1 with action a to state s2 and
-            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
-            
-            Unless a terminal state was reached, two subsequent calls to observe will be for
-            subsequent steps in the same episode. That is, s1 in the second call will be s2
-            from the first call.
-            """
-        # TODO: learn from the observation
-        return
-
-    def training_policy(self, state):
-        """ Returns the index of the action that should be done in state while training the agent.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            training_policy is called once per frame in the game while training
-        """
-        print("state: %s" % state)
-        # TODO: change this to to policy the agent is supposed to use while training
-        # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1) 
-
-    def policy(self, state):
-        """ Returns the index of the action that should be done in state when training is completed.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            policy is called once per frame in the game (30 times per second in real-time)
-            and needs to be sufficiently fast to not slow down the game.
-        """
-        print("state: %s" % state)
-        # TODO: change this to to policy the agent has learned
-        # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1)
-
-
-class FlappyAgentMCAverage(FlappyAgent):
-    def __init__(self):
-        super(FlappyAgentMCAverage, self).__init__()
-
         self.y_pos_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(0, 388)), 15)]
         self.top_y_gap_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(25, 193)), 15)]
         self.velocity_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(-8, 11)), 15)]
-        self.horizontal_distance_next_pipe = [x[-1] for x in numpy.array_split(numpy.array(range(3, 284)), 15)] # ToDo: Maybe refactor and make the first interval a bit bigger.
+        self.horizontal_distance_next_pipe = [x[-1] for x in numpy.array_split(numpy.array(range(3, 284)), 15)]
 
         self.states = list(itertools.product(*[
             self.y_pos_intervals,
@@ -77,21 +21,66 @@ class FlappyAgentMCAverage(FlappyAgent):
             self.velocity_intervals,
         ]))
 
+        self.actions = [0, 1]
         self.Q = {}
         self.pi = {}
         for state in self.states:
             self.pi[state] = random.randint(0, 1)
-            for action in range(0, 2):
-                self.Q[(state, action)] = (0, 0)
+            for action in self.actions:
+                self.Q[(state, action)] = 0
                 self.pi[state] = 0 if random.randint(0, 2) == 0 else 1
 
         self.observations = []
 
-        self.discount = 1
-        self.epsilon = 0.1
-
     def reward_values(self):
         return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
+    
+    def observe(self, s1, a, r, s2, end):
+        print("Observe should be overwritten!")
+        return
+
+    def training_policy(self, state):
+        greedy_action = self.pi[state]
+
+        if random.uniform(0, 1) < (1 - self.epsilon + (self.epsilon / len(self.actions))):
+            return greedy_action
+        else:
+            return [x for x in self.actions if x != greedy_action][0]
+
+    def policy(self, state):
+        return self.pi[state]
+
+    def update_policy(self, s1):
+        if self.Q[(s1, 0)] > self.Q[(s1, 1)]:
+            self.pi[s1] = 0
+        else:
+            self.pi[s1] = 1
+
+    # Gets a state in the original format that PyGame returns.
+    # Both extracts the 4 keys in the problem description
+    # And maps the value to the corresponding interval.
+    # Returns a tuple of:
+    # 1. the current y-position of the bird (player_y component of the game state)
+    # 2. the top y position of the next gap (next_pipe_top_y)
+    # 3. the horizontal distance between bird and next pipe (next_pipe_dist_to_player)
+    # 4. the current velocity of the bird (player_vel)
+    def parse_state(self, state):
+        y_pos = min(self.y_pos_intervals, key=lambda x: abs(x - state['player_y']))
+        top_y_gap = min(self.top_y_gap_intervals, key=lambda x: abs(x - state['next_pipe_top_y']))
+        horizontal_distance_next_pipe = min(self.horizontal_distance_next_pipe,
+                                            key=lambda x: abs(x - state['next_pipe_dist_to_player']))
+        velocity = min(self.velocity_intervals, key=lambda x: abs(x - state['player_vel']))
+
+        return y_pos, top_y_gap, horizontal_distance_next_pipe, velocity
+
+
+class FlappyAgentMCAverage(FlappyAgent):
+    def __init__(self):
+        super(FlappyAgentMCAverage, self).__init__()
+
+        self.discount = 1
+        self.epsilon = 0.1
+        self.method = "Monte_Carlo_Average"
 
     def observe(self, s1, a, r, s2, end):
         self.observations.append((s1, a, r))
@@ -116,69 +105,16 @@ class FlappyAgentMCAverage(FlappyAgent):
 
             self.observations = []
 
-    def training_policy(self, state):
-        actions = [0, 1]
-
-        greedy_action = self.pi[state]
-
-        if random.uniform(0, 1) < 0.95:
-            return greedy_action
-        else:
-            return [x for x in actions if x != greedy_action][0]
-
-    def policy(self, state):
-        return self.pi[state]
-
-    # Gets a state in the original format that PyGame returns.
-    # Both extracts the 4 keys in the problem description
-    # And maps the value to the corresponding interval.
-    # Returns a tuple of:
-    # 1. the current y-position of the bird (player_y component of the game state)
-    # 2. the top y position of the next gap (next_pipe_top_y)
-    # 3. the horizontal distance between bird and next pipe (next_pipe_dist_to_player)
-    # 4. the current velocity of the bird (player_vel)
-    def parse_state(self, state):
-        y_pos = min(self.y_pos_intervals, key=lambda x:abs(x - state['player_y']))
-        top_y_gap = min(self.top_y_gap_intervals, key=lambda x:abs(x - state['next_pipe_top_y']))
-        horizontal_distance_next_pipe = min(self.horizontal_distance_next_pipe, key=lambda x:abs(x - state['next_pipe_dist_to_player']))
-        velocity = min(self.velocity_intervals, key=lambda x:abs(x - state['player_vel']))
-
-        return y_pos, top_y_gap, horizontal_distance_next_pipe, velocity
-
 
 # Flappy Agent that uses Monte-Carlo and a fixed learning rate.
 class FlappyAgentMCLearningRate(FlappyAgent):
     def __init__(self, LearningRate):
         super(FlappyAgentMCLearningRate, self).__init__()
 
-        self.y_pos_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(0, 388)), 15)]
-        self.top_y_gap_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(25, 193)), 15)]
-        self.velocity_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(-8, 11)), 15)]
-        self.horizontal_distance_next_pipe = [x[-1] for x in numpy.array_split(numpy.array(range(3, 284)), 15)]
-
-        self.states = list(itertools.product(*[
-            self.y_pos_intervals,
-            self.top_y_gap_intervals,
-            self.horizontal_distance_next_pipe,
-            self.velocity_intervals,
-        ]))
-
-        self.Q = {}
-        self.pi = {}
-        for state in self.states:
-            self.pi[state] = random.randint(0, 1)
-            for action in range(0, 2):
-                self.Q[(state, action)] = 0
-                self.pi[state] = 0 if random.randint(0, 2) == 0 else 1
-
-        self.observations = []
-
         self.discount = 1
         self.epsilon = 0.1
         self.learning_rate = LearningRate
-
-    def reward_values(self):
-        return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
+        self.method = "Monte_Carlo_Learning_Rate"
 
     def observe(self, s1, a, r, s2, end):
         self.observations.append((s1, a, r))
@@ -196,111 +132,29 @@ class FlappyAgentMCLearningRate(FlappyAgent):
 
             self.observations = []
 
-    def training_policy(self, state):
-        actions = [0, 1]
-
-        greedy_action = self.pi[state]
-
-        if random.uniform(0, 1) < 0.95:
-            return greedy_action
-        else:
-            return [x for x in actions if x != greedy_action][0]
-
-    def policy(self, state):
-        return self.pi[state]
-
-    # Gets a state in the original format that PyGame returns.
-    # Both extracts the 4 keys in the problem description
-    # And maps the value to the corresponding interval.
-    # Returns a tuple of:
-    # 1. the current y-position of the bird (player_y component of the game state)
-    # 2. the top y position of the next gap (next_pipe_top_y)
-    # 3. the horizontal distance between bird and next pipe (next_pipe_dist_to_player)
-    # 4. the current velocity of the bird (player_vel)
-    def parse_state(self, state):
-        y_pos = min(self.y_pos_intervals, key=lambda x: abs(x - state['player_y']))
-        top_y_gap = min(self.top_y_gap_intervals, key=lambda x: abs(x - state['next_pipe_top_y']))
-        horizontal_distance_next_pipe = min(self.horizontal_distance_next_pipe, key=lambda x: abs(x - state['next_pipe_dist_to_player']))
-        velocity = min(self.velocity_intervals, key=lambda x: abs(x - state['player_vel']))
-
-        return y_pos, top_y_gap, horizontal_distance_next_pipe, velocity
-
 
 class FlappyAgentQLearningLearningRate(FlappyAgent):
     def __init__(self, LearningRate):
         super(FlappyAgentQLearningLearningRate, self).__init__()
 
-        self.y_pos_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(0, 388)), 15)]
-        self.top_y_gap_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(25, 193)), 15)]
-        self.velocity_intervals = [x[-1] for x in numpy.array_split(numpy.array(range(-8, 11)), 15)]
-        self.horizontal_distance_next_pipe = [x[-1] for x in numpy.array_split(numpy.array(range(3, 284)), 15)]
-
-        self.states = list(itertools.product(*[
-            self.y_pos_intervals,
-            self.top_y_gap_intervals,
-            self.horizontal_distance_next_pipe,
-            self.velocity_intervals,
-        ]))
-
-        self.Q = {}
-        self.pi = {}
-        self.terminalState = (-1, -1, -1, -1)
-        for state in self.states:
-            self.pi[state] = random.randint(0, 1)
-            for action in range(0, 2):
-                self.Q[(state, action)] = 0
-                self.pi[state] = 0 if random.randint(0, 2) == 0 else 1
-
-        self.observations = []
-
         self.discount = 1
         self.epsilon = 0.1
-
         self.learning_rate = LearningRate
-
-    def reward_values(self):
-        return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
+        self.method = "Q_Learning"
 
     def observe(self, s1, a, r, s2, end):
         self.Q[(s1, a)] = self.Q[(s1, a)] + self.learning_rate * (r + self.discount * self.Q[(s2, self.pi[s2])] - self.Q[(s1, a)])
 
-        if self.Q[(s1, 0)] > self.Q[(s1, 1)]:
-            self.pi[s1] = 0
-        else:
-            self.pi[s1] = 1
+        self.update_policy(s1)
 
 
-    def training_policy(self, state):
-        actions = [0, 1]
-
-        greedy_action = self.pi[state]
-
-        if random.uniform(0, 1) < 0.95:
-            return greedy_action
-        else:
-            return [x for x in actions if x != greedy_action][0]
-
-    def policy(self, state):
-        return self.pi[state]
-
-    # Gets a state in the original format that PyGame returns.
-    # Both extracts the 4 keys in the problem description
-    # And maps the value to the corresponding interval.
-    # Returns a tuple of:
-    # 1. the current y-position of the bird (player_y component of the game state)
-    # 2. the top y position of the next gap (next_pipe_top_y)
-    # 3. the horizontal distance between bird and next pipe (next_pipe_dist_to_player)
-    # 4. the current velocity of the bird (player_vel)
-    def parse_state(self, state):
-        y_pos = min(self.y_pos_intervals, key=lambda x:abs(x - state['player_y']))
-        top_y_gap = min(self.top_y_gap_intervals, key=lambda x:abs(x - state['next_pipe_top_y']))
-        horizontal_distance_next_pipe = min(self.horizontal_distance_next_pipe, key=lambda x:abs(x - state['next_pipe_dist_to_player']))
-        velocity = min(self.velocity_intervals, key=lambda x:abs(x - state['player_vel']))
-
-        return y_pos, top_y_gap, horizontal_distance_next_pipe, velocity
+def save_policy(agent, frames, folder=""):
+    results = {"Frames": frames, "Policy": agent.pi}
+    numpy.save(folder + agent.method + "_" + str(frames) + ".npy", results)
+    print("Saved %s agents policy after training %d frames." % (agent.method, frames))
 
 
-def run_game(nb_episodes, agent):
+def run_game(nb_episodes, agent, frames_to_train=200000, folder=""):
     reward_values = agent.reward_values()
     
     env = PLE(FlappyBird(), fps=30, display_screen=False, force_fps=True, rng=None,
@@ -308,29 +162,28 @@ def run_game(nb_episodes, agent):
     env.init()
 
     elapsed_episodes = 0
-    score = 0
     frames = 0
+    score = 0
     while elapsed_episodes < nb_episodes:
         state = agent.parse_state(env.game.getGameState())
-        frames += 1
         action = agent.training_policy(state)
         reward = env.act(env.getActionSet()[action])
         state2 = agent.parse_state(env.game.getGameState())
 
         agent.observe(state, action, reward, state2, env.game_over())
-        if frames % 50000 == 0:
-            results = {"Frames": frames, "Policy": agent.pi}
-            numpy.save("Monte_Carlo_Average/Average_Frames_" + str(frames) + ".npy", results)
+
+        frames += 1
+        # if frames % 50000 == 0:
+            # save_policy(agent, frames, folder)
         score += reward
-        scores = set()
         if env.game_over():
-            #print("score for this episode: %d" % score)
-            scores.add(score)
             env.reset_game()
-            score = 0
             elapsed_episodes += 1
-            print(elapsed_episodes)
-            if frames >= 2000000:
+            #print(elapsed_episodes)
+            print(score)
+            score = 0
+            if frames >= frames_to_train:
+                save_policy(agent, frames, folder)
                 break
 
 
@@ -342,7 +195,6 @@ def test_policy(nb_episodes, agent):
     scores = []
     score = 0
     while nb_episodes > 0:
-
         state = agent.parse_state(env.game.getGameState())
         action = agent.policy(state)
 
@@ -358,30 +210,10 @@ def test_policy(nb_episodes, agent):
 
     print("Best score: %d" % max(scores))
     print("Average: %f" % (sum(scores)/len(scores)))
-    #return max(scores), (sum(scores)/len(scores))
-    return scores
+    return max(scores), (sum(scores)/len(scores)), scores
 
 
-def iterate_policies(folder, name, total, step):
-    frames = []
-    average_scores = []
-    max_scores = []
-    #scores = []
-    for frame in range(step, total+1, step):
-        file = folder + name + str(frame) + ".npy"
-        print(file)
-        agent_to_test = FlappyAgentQLearningLearningRate(0.1)
-        agent_to_test.pi = numpy.load(file).item()["Policy"]
-        max_score, average = test_policy(4, agent_to_test)
-        #score = test_policy(50, agent_to_test)
-        #frame = str(frame)[:-3] + "k"
-        frames.append(frame)
-        #scores.append(score)
-        average_scores.append(average)
-        max_scores.append(max_score)
-
-    #frames = frames[1::2]
-    #scores = scores[1::2]
+def generate_learning_curve(folder, name, average_scores, max_scores, frames):
     plt.figure(figsize=(20, 10))
     plt.title(folder)
     plt.xlabel("Frames trained on")
@@ -389,18 +221,43 @@ def iterate_policies(folder, name, total, step):
     average_line, = plt.plot(frames, average_scores, label="Average")
     max_line, = plt.plot(frames, max_scores, label="Max")
     plt.legend(handles=[average_line, max_line])
-    #sns.boxplot(x=frames, y=scores)
-    plt.savefig(folder + name + "_boxplot" + ".png")
-    plt.show()
+    plt.savefig(folder + name + ".png")
 
 
-#agent = FlappyAgentQLearningLearningRate(0.1)
-#run_game(20000, agent)
-#pi = numpy.load("Q_Learning/LR_Frames_564.npy").item()
-#agent.pi = pi
-#test_policy(1000, agent)
+def generate_box_plot(folder, name, scores, frames):
+    frames = [str(frame)[:-3] + "k" for frame in frames[1::2]]
+    scores = scores[1::2]
+    plt.figure(figsize=(20, 10))
+    plt.title(folder)
+    plt.xlabel("Frames trained on")
+    plt.ylabel("Score, 50 runs")
+    sns.boxplot(x=frames, y=scores)
+    plt.savefig(folder + name + "box_plot" + ".png")
 
 
-iterate_policies("Q_Learning/", "LR_Frames_", 2000000, 50000)
+def evaluate_policies(folder, name, total, step):
+    frames = []
+    average_scores = []
+    max_scores = []
+    scores = []
+    for frame in range(step, total+1, step):
+        file = folder + name + str(frame) + ".npy"
+        agent_to_test = FlappyAgentQLearningLearningRate(0.1)
+        agent_to_test.pi = numpy.load(file).item()["Policy"]
+        max_score, average, score = test_policy(50, agent_to_test)
+        frames.append(frame)
+        scores.append(score)
+        average_scores.append(average)
+        max_scores.append(max_score)
+
+    generate_box_plot(folder, name, scores, frames)
+    generate_learning_curve(folder, name, average_scores, max_scores, frames)
 
 
+bird = FlappyAgentQLearningLearningRate(0.1)
+run_game(20000, bird, 10000)
+#bird.pi = numpy.load("Q_Learning_10024.npy").item()["Policy"]
+#test_policy(10, bird)
+
+
+#evaluate_policies("MC_SET/", "LR_Frames_", 2000000, 50000)
